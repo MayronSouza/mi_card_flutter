c715f4669a28ac37a8cf98a276c978f4f85adea6
MayronGMZ <mayron.souza@gmz.solutions>
2020-06-07 09:24:56 -0300

    Resolucao desafio de Widgets Column e Row

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.teal,
-          body: Container(),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(        
+        backgroundColor: Colors.yellow,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Container(
+                width: 210.0,
+                color: Colors.teal,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
